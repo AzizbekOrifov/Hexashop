1941dfaf4ed7a10a72439143f8735d68cc4b9bfc
azizbek <a4440404107@example.com>
2023-06-21 13:12:20 +0500

    Hexashop

---
+++ app.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById("burger");
+const Nav = document.querySelector(".nav_mobile");
+
+burger.addEventListener("click", () => {
+  Nav.classList.toggle("show_menu");
+});
